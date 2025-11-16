1e666ccdbd76b75a212550f791b0634715e539c7
Ingo Ruhnke <grumbel@gmail.com>
2025-11-16 21:37:34 +0100

    Update to nixos-25.11

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = rec {
-    nixpkgs.url = "github:NixOS/nixpkgs?ref=nixos-25.05";
+    nixpkgs.url = "github:NixOS/nixpkgs?ref=nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
 
     ipflib-src.url = "http://www.softpres.org/_media/files:ipflib42_linux-x86_64.tar.gz?id=download&cache=cache";
